7056f0e0c012ed416cd4c62c89df8f450e578e2b
Amardip Shrestha <shresthamardip@gmail.com>
2021-04-20 13:13:02 +0200

    Validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -53,8 +53,7 @@ namespace Vidly.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
-        {
-            
+        { 
             if (!ModelState.IsValid)
             {
                 var viewModel = new CustomerFormViewModel
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -27,6 +27,7 @@ namespace Vidly.Models
         public DateTime DateAdded { get; set; }
 
         [Required]
+        [Range(1,20)]
         [Display(Name = "Number in Stock")]
         public int NrInStock { get; set; }
     }
